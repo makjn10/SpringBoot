f40ecdc12554c48bc14948b0da8cf73dbc636d91
makjn10 <10.mayank.jain@gmail.com>
2025-06-21 22:28:51 +0530

    Add more demos

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/mayank/SpringBootPractice/rest/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/mayank/SpringBootPractice/rest/FunRestController.java
@@ -9,6 +9,18 @@ public class FunRestController {
     // expose '/' endpoint that will return 'Hello World!'
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World!";
+        return "Hello World!!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
     }
 }
