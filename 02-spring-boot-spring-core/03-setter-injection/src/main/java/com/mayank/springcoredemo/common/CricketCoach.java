6949df2eadfa364f356493e9a6331f0bb0d48d49
makjn10 <10.mayank.jain@gmail.com>
2025-06-22 19:17:28 +0530

    Add setter injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/mayank/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.mayank.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practise fast bowling for 15 minutes :-)";
+    }
+}
