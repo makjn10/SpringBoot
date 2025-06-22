37fb1d330d6af958e9165eb60a2a47128724a4b6
makjn10 <10.mayank.jain@gmail.com>
2025-06-22 17:16:41 +0530

    Add spring core

--- 01-spring-boot-overview/06-properties-demo/src/main/java/com/mayank/SpringBootPractice/rest/FunRestController.java
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/mayank/SpringBootPractice/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.mayank.SpringBootPractice.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for teaminfo
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach name : " + coachName + " , team name : " + teamName;
+    }
 
     // expose '/' endpoint that will return 'Hello World!'
     @GetMapping("/")
